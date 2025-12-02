105327c5a5cb8587708b2f7afe68b0001143ff11
Praneetha-07 <ravuripraneetha7@gmail.com>
2025-12-02 21:49:18 +0530

    Added * path for any invalid routes

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ function App() {
         />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </AuthProvider>
   );
